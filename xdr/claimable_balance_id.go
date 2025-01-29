c8c4cafb6600ba511f167146f3fddcda49abc54c
Simon Chow <simon.chow@stellar.org>
2025-01-29 18:02:53 -0500

    xdrill functions for ledger entry changes

--- xdr/claimable_balance_id.go
+++ xdr/claimable_balance_id.go
@@ -1,5 +1,7 @@
 package xdr
 
+import "encoding/base64"
+
 func (e *EncodingBuffer) claimableBalanceCompressEncodeTo(cb ClaimableBalanceId) error {
 	if err := e.xdrEncoderBuf.WriteByte(byte(cb.Type)); err != nil {
 		return err
@@ -11,4 +13,15 @@ func (e *EncodingBuffer) claimableBalanceCompressEncodeTo(cb ClaimableBalanceId)
 	default:
 		panic("Unknown type")
 	}
+}
+
+// MarshalBinaryBase64 marshals XDR into a binary form and then encodes it
+// using base64.
+func (c ClaimableBalanceId) MarshalBinaryBase64() (string, error) {
+	b, err := c.MarshalBinary()
+	if err != nil {
+		return "", err
+	}
+
+	return base64.StdEncoding.EncodeToString(b), nil
 }
--- xdr/pool_id.go
+++ xdr/pool_id.go
@@ -3,6 +3,7 @@ package xdr
 import (
 	"bytes"
 	"crypto/sha256"
+	"encoding/base64"
 
 	"github.com/stellar/go/support/errors"
 )
@@ -27,4 +28,15 @@ func NewPoolId(a, b Asset, fee Int32) (PoolId, error) {
 		return PoolId{}, errors.Wrap(err, "failed to build liquidity pool id")
 	}
 	return sha256.Sum256(buf.Bytes()), nil
+}
+
+// MarshalBinaryBase64 marshals XDR into a binary form and then encodes it
+// using base64.
+func (p PoolId) MarshalBinaryBase64() (string, error) {
+	b, err := p.MarshalBinary()
+	if err != nil {
+		return "", err
+	}
+
+	return base64.StdEncoding.EncodeToString(b), nil
 }
